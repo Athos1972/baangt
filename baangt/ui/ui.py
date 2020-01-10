775dc4cb01820b36340acd193503bab1faf4135f
bernhardbuhl <buhl@buhl-consulting.com.cy>
2020-01-10 20:33:59 +0100

    UI with Ini-File to save last directory and Display of JSON-File Contents for Globals

--- baangt/ui/ui.py
+++ baangt/ui/ui.py
@@ -1,40 +1,66 @@
 import PySimpleGUI as sg
+import glob
+import os
+import subprocess
+import configparser
+import baangt.base.GlobalConstants as GC
+from baangt.base.utils import utils
+import logging
+
+logger = logging.getLogger("pyC")
+
 
 class UI:
+    """
+    Provides a simple UI for Testrun-Execution
+    """
     def __init__(self):
-        self.directory = None
+        # Todo 1: Save last used directory to local config File
+        # Todo 2: Use the parameters from Globals and provide an option to change them
         self.configFile = None
         self.configFiles = []
         self.testRunFile = None
         self.testRunFiles = []
+        self.configContents = {}
         self.window = None
+
+        self.directory = None
+        self.readConfig()
+        self.getConfigFilesInDirectory()
 
         self.startWindow()
 
+    def getLayout(self):
+        lLayout = [[sg.Text("Select Directory, Testrun and Global settings to use:")],
+                   [sg.Text("Directory", size=(15,1)),
+                    sg.In(key="-directory-", size=(30,1), enable_events=True, default_text=self.directory),
+                    sg.FolderBrowse(initial_folder=os.getcwd(), enable_events=True)],
+                   [sg.Text("TestRun", size=(15,1)),
+                    sg.InputCombo(self.testRunFiles, key="testRunFile", default_value=self.testRunFile, size=(30,1))],
+                   [sg.Text("Global Settings", size=(15,1)),
+                    sg.InputCombo(self.configFiles, key="configFile", default_value=self.configFile, enable_events=True, size=(30,1))]]
+
+        if self.configContents:
+            lLayout.append([sg.Text("-"*10 + f"Settings of file {self.configFile}" + "-"*10)])
+            for key, value in self.configContents.items():
+                lLayout.append([sg.Text(key, size=(25,1)), sg.In(key=key, size=(30,1), default_text=value)])
+            for i in range(0,4):
+                lLayout.append([sg.In(key=f"newField{i}", size=(25,1)), sg.In(key=f"newValue{i}", size=(30,1))])
+
+        lLayout.append([sg.Button('Submit'), sg.Button('Exit'), sg.Button("Execute TestRun")])
+
+        return lLayout
 
     def startWindow(self):
         sg.theme("TanBlue")
 
-        lLayout = [[sg.Text("Select Directory, Testrun and Global settings to use:")],
-                   [sg.Text("Directory", size=(20,1)),
-                    sg.In(key="-directory-", size=(20,1)), sg.FolderBrowse()],
-                   [sg.Text("TestRun", size=(20,1)),
-                    sg.InputCombo(("CV1", "CV2"), key="testRunFile", size=(20,1))],
-                   [sg.Text("Global Settings", size=(20,1)),
-                    sg.InputCombo(("GS1", "GS2"), key="configFile", size=(20,1))],
-                   [sg.Submit(), sg.Button('Exit')]]
-
-        self.window = sg.Window("Baangt interactive Starter", layout=lLayout)
+        self.window = sg.Window("Baangt interactive Starter", layout=self.getLayout())
         lWindow = self.window
         lWindow.finalize()
-        lWindow["_franzi_"].update("jetztzeit")
-        lWindow["configFile"].update(values=["susi", "strolchi"])
-        lWindow["-directory-"].update(value=self.directory)
 
         while True:
-
             lEvent, lValues = lWindow.read()
-            print(lValues["configFile"])
+            print(lValues.get("configFile"))
             if lEvent == "Exit":
                 break
             if lValues.get('-directory-') != self.directory:
@@ -42,14 +68,71 @@ class UI:
                 self.getConfigFilesInDirectory()
                 lWindow['configFile'].update(values=self.configFiles, value="")
                 lWindow['testRunFile'].update(values=self.testRunFiles, value="")
-            text_input = lValues.get('franzi')
-            file_chosen = lValues["configFile"]
-            sg.popup(f"You entered: {text_input} and chose file {file_chosen}")
+                lValues['configFile'] = ""
 
-        lWindow.close
+            if lValues["configFile"]:
+                if lValues["configFile"] != self.configFile:
+                    self.configFile = lValues['configFile']
+                    self.readContentsOfGlobals()
+                    lWindow.close()
+                    self.window = sg.Window("Baangt interactive Starter", layout=self.getLayout())
+                    lWindow = self.window
+                    lWindow.finalize()
+
+            if lValues["testRunFile"]:
+                self.testRunFile = lValues["testRunFile"]
+
+            if lEvent == "Execute TestRun":
+                self.runTestRun()
+
+        self.saveConfig()
+
+    def runTestRun(self):
+        if not self.configFile:
+            sg.popup_cancel("No Config File selected - can't run")
+            return
+        if not self.testRunFile:
+            sg.popup_cancel("No Testrun File selected - can't run")
+            return
+        runCmd = f"python baangt.py --run='{self.directory}/{self.testRunFile}' " \
+                 f"--globals='{self.directory}/{self.configFile}'"
+        logger.info(f"Running command: {runCmd}")
+        p = subprocess.run(runCmd, shell=True)
+        sg.popup_ok("Testrun finished")
 
     def getConfigFilesInDirectory(self):
         """Reads *.JSON-Files from directory given in self.directory and builds 2 lists (Testrunfiles and ConfiFiles"""
+        self.configFiles = []
+        self.testRunFiles = []
+        lcwd = os.getcwd()
+        os.chdir(self.directory)
+        fileList = glob.glob("*.json")
+        fileList.extend(glob.glob("*.xlsx"))
+        for file in fileList:
+            if file[0:4].lower() == 'glob':
+                self.configFiles.append(file)
+            else:
+                self.testRunFiles.append(file)
+            pass
+
+        os.chdir(lcwd)
+
+    def readContentsOfGlobals(self):
+        self.configContents = utils.openJson(self.directory + "/" + self.configFile)
+
+    def saveConfig(self):
+        config = configparser.ConfigParser()
+        config["DEFAULT"] = {"path": self.directory}
+        with open("baangt.ini", "w") as configFile:
+            config.write(configFile)
+
+    def readConfig(self):
+        config = configparser.ConfigParser()
+        try:
+            config.read("baangt.ini")
+            self.directory = config["DEFAULT"]['path']
+        except Exception as e:
+            pass
 
 
 
