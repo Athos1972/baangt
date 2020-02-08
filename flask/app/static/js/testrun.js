9a6feb811981a7c6577ef2bc9db63ca55c6cc0b0
aguryev <aguryev@ukr.net>
2020-02-08 23:08:15 +0200

    chips for multiselect field

--- flask/app/static/js/testrun.js
+++ flask/app/static/js/testrun.js
@@ -63,3 +63,26 @@
             request.send();
         };
     }
+
+    // add chip
+    function add_chip(e) {
+        var chip_area = document.getElementById('testcaseSequences');
+        var new_chip = document.createElement('div');
+        new_chip.setAttribute('class', 'chip mr-1');
+        new_chip.setAttribute('data-id', e.options[e.selectedIndex].value);
+        new_chip.innerHTML = `<small>${e.options[e.selectedIndex].text}</small><span class="closebtn" onclick="delete_chip(this.parentElement)">&times;</span>`;
+        e.options[e.selectedIndex].disabled = true;
+        e.selectedIndex = 0;
+        chip_area.appendChild(new_chip);
+    }
+
+    function delete_chip(e) {
+        var selector = document.getElementById('testcase_sequences_2');
+        for (var i = 1; i < selector.length; i++) {
+            if (selector.options[i].value == e.dataset['id']) {
+                selector.options[i].disabled = false;
+            }
+        }
+        e.style.display='none';
+
+    }
