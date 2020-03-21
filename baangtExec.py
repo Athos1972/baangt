a44fda9f1e5d2a1b5077c23565934baea5eddae8
aguryev <aguryev@ukr.net>
2020-03-21 20:42:06 +0200

    exec: ubuntu

---
+++ baangtExec.py
@@ -0,0 +1,5 @@
+from baangt.base.CliAndInteractive import run
+from multiprocessing import freeze_support
+
+freeze_support()
+run()
---
+++ exec.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+pyinstaller --clean --onedir \
+	--distpath ubuntu/ \
+	--workpath ubuntu/build \
+	--specpath ubuntu \
+	--name baangt \
+	--add-data '../baangt/ressources/baangtLogo2020Small.png:ressources' \
+	--add-data '../browserDrivers/geckodriver:geckodriver' \
+	baangtExec.py
