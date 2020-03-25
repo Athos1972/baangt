04680dfb08b28b9ef19399004768c04bf6f582bb
bernhardbuhl <buhl@buhl-consulting.com.cy>
2020-03-25 06:27:20 +0100

    Executable for MacOS. Updated requirements.txt to downgrade library for pyinstaller

---
+++ execMac.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+pyinstaller --clean --onedir \
+	--distpath exec_mac/ \
+	--workpath exec_mac/build \
+	--specpath exec_mac \
+	--name baangt \
+	--add-data '../baangt/ressources/baangtLogo2020Small.png:ressources' \
+	--add-data '../browserDrivers/geckodriver:geckodriver' \
+	baangtExec.py
