f0dee629e7cdb16201752214d06e2b0654284da6
BernhardSurface <buhl@buhl-consulting.com.cy>
2020-03-29 16:31:09 +0300

    Windows Executable

--- execWindows.bat
+++ execWindows.bat
@@ -1,18 +1,11 @@
 
-pyinstaller windows/baangtWindows.spec
-
-rem              datas=[('../baangt/ressources/baangtLogo2020Small.png', 'ressources'),
-rem                    ('..\examples\', 'examples'),
-rem                    ('..\browsermob-proxy','browsermob-proxy\')],
-
-
-rem --add-data "..\examples\;examples" --add-data "..\browsermob-proxy;browsermob-proxy\"
+pyinstaller --noconfirm windows/baangtWindows.spec
 
 rem Remove Screenshots and Logs ^
-rm -r windows/baangt/examples/Screenshots
-rm -r windows/baangt/examples/Logs
+rm -r dist/baangt/examples/Screenshots
+rm -r dist/baangt/examples/Logs
 
 rem Create ZIP-file
 mkdir executables
 rm executables/baangt_windows_executable.zip
-rem powershell Compress-Archive windows/. executables/baangt_windows_executable.zip
+powershell Compress-Archive dist/baangt/. executables/baangt_windows_executable.zip
