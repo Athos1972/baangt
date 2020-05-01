ee77f7f8b4e352d8685d1b9ca6f15594b07b3bd7
Bernhard <buhl@buhl-consulting.com.cy>
2020-05-01 19:40:07 +0300

    Windows build instructions updated

--- execWindows.bat
+++ execWindows.bat
@@ -1,11 +1,16 @@
+rm -r dist
+rm -r build
+rm -r executables
 
 pyinstaller --noconfirm windows/baangtWindows.spec
 
 rem Remove Screenshots and Logs ^
 rm -r dist/baangt/examples/Screenshots
 rm -r dist/baangt/examples/Logs
+rm -r dist/baangt/examples/1testoutput
+rm -r dist/baangt/Logs
 
 rem Create ZIP-file
-rem mkdir executables
-rem rm executables/baangt_windows_executable.zip
-rem powershell Compress-Archive dist/baangt/. executables/baangt_windows_executable.zip
+mkdir executables
+rm executables/baangt_windows_executable.zip
+powershell Compress-Archive dist/baangt/. executables/baangt_windows_executable.zip
