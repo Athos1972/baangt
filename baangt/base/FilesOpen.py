ccb30166030cca65dcbb6660ef2cc8491edafc99
bernhardbuhl <buhl@buhl-consulting.com.cy>
2020-05-05 10:55:50 +0200

    Unit-Tests and Class for FilesOpen.py

--- baangt/base/FilesOpen.py
+++ baangt/base/FilesOpen.py
@@ -2,15 +2,17 @@ import platform
 import os
 import subprocess
 
-
 def open(filenameAndPath: str):
-
     """Will open the given file with the Operating system default program.
 
     param
 
     filenameAndPath : Complete absolute path to file.
-    return : True if sucessfully open. False if file doesn't exists or operating system doesn't have default program."""
+    return : True if sucessfully open. False if file doesn't exists or operating system doesn't have default program.
+    """
+
+    if not isinstance(filenameAndPath, str):
+        filenameAndPath = str(filenameAndPath)
 
     if not os.path.exists(filenameAndPath):
         return False
@@ -21,14 +23,40 @@ def open(filenameAndPath: str):
         except:
             return False
     elif platform.system() == "Linux":
-        status = subprocess.call(["xdg-open", filenameAndPath])
+        status = subprocess.call(["xdg-open", str(filenameAndPath)])
         if status == 0:
             return True
         else:
             return False
     elif platform.system() == "Darwin":
-        status = os.system("open "+filenameAndPath)
+        status = os.system("open " + str(filenameAndPath))
         if status == 0:
             return True
         else:
             return False
+
+
+class FilesOpen:
+    """
+    Class is called from UI and will open the corresponding file-type in the os-specific application.
+    """
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def openTestRunDefinition(filenameAndPath):
+        return open(filenameAndPath=filenameAndPath)
+
+    @staticmethod
+    def openResultFile(filenameAndPath):
+        return open(filenameAndPath=filenameAndPath)
+
+    @staticmethod
+    def openLogFile(filenameAndPath):
+        return open(filenameAndPath=filenameAndPath)
+
+
+
+
+
+
