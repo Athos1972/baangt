3969e540ef94bf626501809562a639a6d1eaa81e
bernhardbuhl <buhl@buhl-consulting.com.cy>
2020-05-06 17:42:24 +0200

    Updated tests to keep data files after test

--- tests/test_TestDataGenerator.py
+++ tests/test_TestDataGenerator.py
@@ -2,53 +2,71 @@ from baangt.TestDataGenerator.TestDataGenerator import TestDataGenerator
 import xlrd
 import os
 import csv
+from pathlib import Path
+import pytest
 import baangt.base.GlobalConstants as GC
+from xlsxwriter.exceptions import FileCreateError
 
 # Create an instance of TestDataGenerator object with sample input file
 testDataGenerator = TestDataGenerator("0TestInput/RawTestData.xlsx")
+testOutput1000xls = str(Path(os.getcwd()).joinpath("1TestResults").joinpath("output1000.xlsx"))
+testOutput3000csv = str(Path(os.getcwd()).joinpath("1TestResults").joinpath("output3000.csv"))
+testOutputFullxls = str(Path(os.getcwd()).joinpath("1TestResults").joinpath("outputFull.xlsx"))
+testOutputFullcsv = str(Path(os.getcwd()).joinpath("1TestResults").joinpath("outputFull.csv"))
+
+
+def removeFile(file):
+    try:
+        os.remove(file)
+    except Exception as e:
+        pass
 
 
 def test_write_excel_1000():
     # Tests write method of TestDataGenerator object with 1000 random data in excel.
-    testDataGenerator.write(batch_size=1000)
-    xl_book = xlrd.open_workbook(GC.TESTDATAGENERATOR_OUTPUTFILE_XLSX)
+    removeFile(testOutput1000xls)
+    testDataGenerator.write(batch_size=1000, outputfile=testOutput1000xls)
+    xl_book = xlrd.open_workbook(testOutput1000xls)
     xl_sheet = xl_book.sheet_by_index(0)
     assert xl_sheet.nrows == 1001
     print("Test 1000 random data successful.")
-    os.remove(GC.TESTDATAGENERATOR_OUTPUTFILE_XLSX)
 
 
 def test_write_csv_3000():
     # Tests write method of TestDataGenerator object with 3000 random data in csv.
-    testDataGenerator.write(OutputFormat="csv", batch_size=3000)
+    removeFile(testOutput3000csv)
+    testDataGenerator.write(OutputFormat="csv", batch_size=3000, outputfile=testOutput3000csv)
     data = []
-    with open(GC.TESTDATAGENERATOR_OUTPUTFILE_CSV, 'r') as raw_file:
+    with open(testOutput3000csv, 'r') as raw_file:
         raw = csv.reader(raw_file)
         for row in raw:
             data.append(row)
     assert len(data) == 3001
     print("Test 3000 random data successful.")
-    os.remove(GC.TESTDATAGENERATOR_OUTPUTFILE_CSV)
 
 
 def test_write_excel_all():
+    removeFile(testOutputFullxls)
     # Takes too much time!!!!!
     # Tests write_excel method of TestDataGenerator object with all data.
-    testDataGenerator.write()
-    xl_book = xlrd.open_workbook(GC.TESTDATAGENERATOR_OUTPUTFILE_XLSX)
+    testDataGenerator.write(outputfile=testOutputFullxls)
+    xl_book = xlrd.open_workbook(testOutputFullxls)
     xl_sheet = xl_book.sheet_by_index(0)
     print(f"Total data in excel file = {str(xl_sheet.nrows)}")
-    os.remove(GC.TESTDATAGENERATOR_OUTPUTFILE_XLSX)
 
 
 def test_write_csv_all():
     # Tests write_csv method of TestDataGenerator
-    testDataGenerator.write(OutputFormat="csv")
+    removeFile(testOutputFullcsv)
+    testDataGenerator.write(OutputFormat="csv", outputfile=testOutputFullcsv)
     data = []
-    with open(GC.TESTDATAGENERATOR_OUTPUTFILE_CSV, 'r') as raw_file:
+    with open(testOutputFullcsv, 'r') as raw_file:
         raw = csv.reader(raw_file)
         for row in raw:
             data.append(row)
     print(f"Total data in excel file = {str(len(data))}")
-    os.remove(GC.TESTDATAGENERATOR_OUTPUTFILE_CSV)
 
+
+def test_write_to_wrong_Path():
+    with pytest.raises(FileCreateError):
+        testDataGenerator.write(outputfile="/franzi/fritzi/hansi.xlsx", batch_size=100)
