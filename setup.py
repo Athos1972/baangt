0bf7b727029fe6f2d60a03639ddc4218401f207b
bernhardbuhl <buhl@buhl-consulting.com.cy>
2020-05-09 16:30:55 +0200

    2020.5.2rc5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 
     setuptools.setup(
         name="baangt",
-        version="2020.5.1rc5",
+        version="2020.5.2rc5",
         author="Bernhard Buhl",
         author_email="info@baangt.org",
         description="Open source Test Automation Suite for MacOS, Windows, Linux",
