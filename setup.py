8f400a0a70c2e9757bec4e06c8d88aa29f2d8d94
bernhardbuhl <buhl@buhl-consulting.com.cy>
2020-05-16 17:54:49 +0200

    Bump version: 0.9.1 → 0.9.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 
     setuptools.setup(
         name="baangt",
-        version="0.9.1",
+        version="0.9.2",
         author="Bernhard Buhl",
         author_email="info@baangt.org",
         description="Open source Test Automation Suite for MacOS, Windows, Linux",
