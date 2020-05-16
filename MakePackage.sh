5bcf66aff46f7530d7be5f8084dfbd7cd7414b88
bernhardbuhl <buhl@buhl-consulting.com.cy>
2020-05-16 17:54:34 +0200

    Pre-Release adjustments

--- MakePackage.sh
+++ MakePackage.sh
@@ -4,6 +4,6 @@ rm -R dist/baangtIA
 rm -R dist/baangt.app
 rm -R dist/baangtIA.app
 python3 setup.py sdist bdist_wheel
-python3 -m pip install --user --upgrade twine
+python3 -m pip install --upgrade twine
 python3 -m twine upload dist/*
 rm -R build/
