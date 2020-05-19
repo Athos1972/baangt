05a187bff71642a36f9943b86b811c0ca8834bb1
Akash Singh <randomthings2206@gmail.com>
2020-05-19 21:19:39 +0530

    Removed debugging stuff

--- baangt/TestCaseSequence/TestCaseSequenceMaster.py
+++ baangt/TestCaseSequence/TestCaseSequenceMaster.py
@@ -84,16 +84,11 @@ class TestCaseSequenceMaster:
                                                    testcaseSequence=self.testCases,
                                                    **kwargs)
                 process.one_sequence(results)
-        print(parallelInstances)
+
         # Create and runconcurrent threads
-        lis = []
-        for num in range(parallelInstances):
-            print(num)
-            lis.append(gevent.spawn(single_thread, num))
-            print(num)
-        print(lis)
-        print(len(lis))
-        threads = gevent.joinall(lis, timeout=1200)
+        threads = gevent.joinall([
+            gevent.spawn(single_thread, num) for num in range(parallelInstances)
+        ], timeout=1200)
 
         # after joining all threads
         while not results.empty():
