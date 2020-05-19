0530c6ad051813aa1870e6468079118f7fa2d3a9
Akash Singh <randomthings2206@gmail.com>
2020-05-19 22:43:41 +0530

    Pytest for mokey.patch_all()

---
+++ pytest_patch.py
@@ -0,0 +1,9 @@
+from gevent import monkey
+monkey.patch_all()
+
+import pytest
+import re
+import sys
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+    sys.exit(pytest.main())>
