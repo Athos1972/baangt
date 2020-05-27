ea2fdb3f1ef07fc2cc869ea65eae2d95549a81eb
bernhardbuhl <buhl@buhl-consulting.com.cy>
2020-05-27 17:59:01 +0200

    Bump version: 0.9.10 → 0.9.11

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, Bernhard Buhl'
 author = 'Bernhard Buhl'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.10'
+release = '0.9.11'
 
 
 # -- General configuration ---------------------------------------------------
