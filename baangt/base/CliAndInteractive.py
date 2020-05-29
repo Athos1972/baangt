5302df1636760ea3c0923fecf86f451ac5fa3528
aguryev <aguryev@ukr.net>
2020-05-30 00:05:51 +0300

    Dashboard generator

--- baangt/base/CliAndInteractive.py
+++ baangt/base/CliAndInteractive.py
@@ -7,6 +7,8 @@ from PyQt5 import QtWidgets
 # from baangt.ui.pyqt.uimain import MainWindow
 from baangt.ui.pyqt.uimain import MainController
 from baangt.base.RuntimeStatistics import Statistic
+from baangt.reports import Reports
+
 def print_args():
     print("""
 Call: python baangtIA.py --parameters 
@@ -30,7 +32,8 @@ def args_read(l_search_parameter):
         opts, args = getopt.getopt(l_args, "", ["run=",
                                                 "globals=",
                                                 "reloadDrivers=",
-                                                "gui="
+                                                "gui=",
+                                                "reports=",
                                                 ])
     except getopt.GetoptError as err_det:
         print("Error in reading parameters:" + str(err_det))
@@ -78,6 +81,10 @@ def run():
         lDriver.downloadDriver(GC.BROWSER_FIREFOX)
         lDriver.downloadDriver(GC.BROWSER_CHROME)
         print("Latest versions of drivers for Firefox and Chrome were downloaded")
+    elif args_read("reports"):
+        r = Reports()
+        r.show_dashboard()
+
     else:
         app = QtWidgets.QApplication(sys.argv)
         controller = MainController()
