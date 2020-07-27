43621a6b5026839ddd0e83472412a590d564bc4f
bernhardbuhl <buhl@buhl-consulting.com.cy>
2020-07-27 07:30:25 +0200

    IBAN via Faker if Schwifty fails (on Linux)

--- baangt/base/IBAN.py
+++ baangt/base/IBAN.py
@@ -26,11 +26,15 @@ class IBAN:
         for n in range(laenge):
             digits.append(random.randrange(0, 10))
         digits = "".join(str(x) for x in digits)
-        return str(schwifty.IBAN.generate(country_code=self.bankLand,
-                                          bank_code=self.bankLeitZahl,
-                                          account_code=digits))
-        return Faker().fakerProxy(fakerMethod="iban")
+        # Schwifty doesn't work on Ubuntu
+        try:
+            lReturn = str(schwifty.IBAN.generate(country_code=self.bankLand,
+                                              bank_code=self.bankLeitZahl,
+                                              account_code=digits))
+        except Exception:
+            lReturn = Faker().fakerProxyIBAN("AT")
 
+        return lReturn
 
 if __name__ == '__main__':
     l = IBAN()
